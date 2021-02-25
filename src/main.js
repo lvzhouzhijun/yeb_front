181057552cf638d914a7066a20851deae633ef6a
zhouzhi <1175974811@qq.com>
2021-02-25 15:53:02 +0800

    ElementUI 安装导入，登录页面的编写

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = true
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 安装 ElementUI
+Vue.use(ElementUI);
+Vue.config.productionTip = false
 
 new Vue({
   router,
