e9725930eb21afbcacf050a3545a740b3008698c
zhouzhi <1175974811@qq.com>
2021-03-10 15:22:08 +0800

    导航菜单功能实现

---
+++ src/views/Test1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Test1
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Test'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Test2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Test2
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Test'
+  }
+</script>
+
+<style scoped>
+
+</style>
